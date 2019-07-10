fde84aa51702b9797d2e62e8d9cc77e007d84f57
LuciaVillamizarFuentes <52726302+LuciaVillamizarFuentes@users.noreply.github.com>
2019-07-10 18:15:20 -0500

    cambios

--- ing_sw_2019/src/ing_sw_2019/Ing_sw_2019.java
+++ ing_sw_2019/src/ing_sw_2019/Ing_sw_2019.java
@@ -14,7 +14,13 @@ public class Ing_sw_2019 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println(factorial(5));
     }
-    
+    static int factorial(int numero){
+          if ( numero <= 1 ) {
+              return 1;
+          } else {
+              return numero*factorial(numero-1);
+          }
+     }
 }
